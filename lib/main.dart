48e597ec564a651af8257e88ced8d0ebdb0397fa
Sojeb Chandra <sksojib6860@gmail.com>
2026-06-02 22:20:05 +0600

    add .env file security reason

--- lib/main.dart
+++ lib/main.dart
@@ -1,10 +1,12 @@
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_dotenv/flutter_dotenv.dart';
 
 import 'expense_tracker_app.dart';
 import 'firebase_options.dart';
 
 Future<void> main() async {
+  await dotenv.load();
   WidgetsFlutterBinding.ensureInitialized();
   await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform);
   runApp(const ExpenseTrackerApp());
